96b68a93bf1ee1fc67783220658b7c9e31b5d66e
Berslancoder <bbhizliok@gmail.com>
2021-12-20 16:12:26 +0300

    Login tarafına bazı düzenlemeler ve Admin paneli denemesi

--- WebProgramlamaProje/Controllers/HomeController.cs
+++ WebProgramlamaProje/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -9,6 +10,7 @@ using WebProgramlamaProje.Models;
 
 namespace WebProgramlamaProje.Controllers
 {
+    
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -23,6 +25,7 @@ namespace WebProgramlamaProje.Controllers
             return View();
         }
 
+        [Authorize]
         public IActionResult Privacy()
         {
             return View();
